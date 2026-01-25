565b899f54266a8f533b409a1841550030ea8357
Ishaan Agarwal <73911978+IshaanA-projects@users.noreply.github.com>
2026-01-25 19:01:26 +0000

    Add standardization for turbulence calculations
    Standardize TurbulenceX, TurbulenceY, and TurbulenceZ values.

--- Data/Data analysis.py
+++ Data/Data analysis.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv(r"log.csv")
@@ -9,6 +8,10 @@ df = df[["x", "y", "z", "temp", "pressure"]]
 df["TurbulenceX"] = df["x"].rolling(20).std()
 df["TurbulenceY"] = df["y"].rolling(20).std()
 df["TurbulenceZ"] = df["z"].rolling(20).std()
+
+df["TurbulenceX"] = (df["TurbulenceX"] - df["TurbulenceX"].mean()) / df["TurbulenceX"].std()
+df["TurbulenceY"] = (df["TurbulenceY"] - df["TurbulenceY"].mean()) / df["TurbulenceY"].std()
+df["TurbulenceZ"] = (df["TurbulenceZ"] - df["TurbulenceZ"].mean()) / df["TurbulenceZ"].std()
 
 
 plt.plot(df["TurbulenceX"], label = "x")
